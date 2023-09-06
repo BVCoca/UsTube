9c9a0d618f6235cd0f27c5d3018d75192b7d6a06
Benjamin Vaique <vaique.benjamin@gmail.com>
2023-09-06 13:08:31 +0200

    Modification script converter date

--- public/assets/js/dateConverter.js
+++ public/assets/js/dateConverter.js
@@ -1,18 +1,52 @@
 // Récupérez tous les éléments p par classe
-const dateElements = document.querySelectorAll(".card-text");
+const dateElements = document.querySelectorAll(".date");
 
 // Parcourez chaque élément p
 dateElements.forEach((dateElement) => {
   const id = dateElement.id.split("-")[1]; // Récupérez l'ID de la vidéo
 
+  const dateText = dateElement.textContent.trim();
+  const dateParts = dateText.split("/");
+
   // Récupérez la date de création à partir de l'élément p
-  const createdAt = new Date(dateElement.textContent.trim());
+  const createdAt = new Date(`${dateParts[1]}-${dateParts[0]}-${dateParts[2]}`);
 
-  // Calculez la différence entre la date actuelle et la date de création
+  // Calculez la différence entre la date actuelle et la date de création en secondes
   const currentDate = new Date();
-  const timeDifference = currentDate - createdAt;
-  const hoursDifference = Math.floor(timeDifference / (1000 * 60 * 60));
+  const timeDifference = Math.floor((currentDate - createdAt) / 1000); // Convertir en secondes
 
-  // Mettez à jour le contenu de l'élément p avec la date relative
-  dateElement.textContent = `Il y a ${hoursDifference} heures depuis ${createdAt.toLocaleDateString()} à ${createdAt.toLocaleTimeString()}`;
+  if (timeDifference >= 31536000) {
+    // Plus d'un an, affichez la date en années
+    const yearsDifference = Math.floor(timeDifference / 31536000);
+    dateElement.textContent = `Il y a ${yearsDifference} an${
+      yearsDifference > 1 ? "s" : ""
+    }`;
+  } else if (timeDifference >= 2592000) {
+    // Plus d'un mois, affichez la date en mois
+    const monthsDifference = Math.floor(timeDifference / 2592000);
+    dateElement.textContent = `Il y a ${monthsDifference} mois`;
+  } else if (timeDifference >= 86400) {
+    // Plus d'un jour, affichez la date en jours
+    const daysDifference = Math.floor(timeDifference / 86400);
+    dateElement.textContent = `Il y a ${daysDifference} jour${
+      daysDifference > 1 ? "s" : ""
+    }`;
+  } else if (timeDifference >= 3600) {
+    // Plus d'une heure, affichez la date en heures
+    const hoursDifference = Math.floor(timeDifference / 3600);
+    dateElement.textContent = `Il y a ${hoursDifference} heure${
+      hoursDifference > 1 ? "s" : ""
+    }`;
+  } else if (timeDifference >= 60) {
+    // Plus d'une minute, affichez la date en minutes
+    const minutesDifference = Math.floor(timeDifference / 60);
+    dateElement.textContent = `Il y a ${minutesDifference} minute${
+      minutesDifference > 1 ? "s" : ""
+    }`;
+  } else {
+    // Moins d'une minute, affichez la date en secondes
+    dateElement.textContent = `Il y a ${timeDifference} seconde${
+      timeDifference > 1 ? "s" : ""
+    }`;
+  }
 });
