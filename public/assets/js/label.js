d90b90785bc39b2de0cab1c7a2150d62a328f583
Benjamin Vaique <vaique.benjamin@gmail.com>
2023-09-07 14:42:31 +0200

    Correction du script (textarea)

--- public/assets/js/label.js
+++ public/assets/js/label.js
@@ -1,5 +1,6 @@
 const labels = document.querySelectorAll("label[for]");
 const inputElements = document.querySelectorAll("input");
+const textAreaElements = document.querySelectorAll("textarea");
 
 inputElements.forEach((inputElement) => {
   inputElement.addEventListener("input", function () {
@@ -7,6 +8,21 @@ inputElements.forEach((inputElement) => {
     const inputId = inputElement.getAttribute("id");
 
     const label = document.querySelector(`label[for=${inputId}]`);
+
+    if (texteSaisi.length > 0) {
+      label.style.display = "none";
+    } else {
+      label.style.display = "block";
+    }
+  });
+});
+
+textAreaElements.forEach((textAreaElement) => {
+  textAreaElement.addEventListener("input", function () {
+    const texteSaisi = textAreaElement.value;
+    const textAreaId = textAreaElement.getAttribute("id");
+
+    const label = document.querySelector(`label[for=${textAreaId}]`);
 
     if (texteSaisi.length > 0) {
       label.style.display = "none";
