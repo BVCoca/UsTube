7a01c524b09f6bf4ad0c78ee03785c053990d7d9
Benjamin Vaique <vaique.benjamin@gmail.com>
2023-09-08 11:32:00 +0200

    Modif admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,18 +2,30 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Video;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminController extends AbstractController
 {
+    private $manager;
+
+    public function __construct(EntityManagerInterface $manager)
+    {
+        $this->manager = $manager;
+    }
+
     #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
+        $user = $this->getUser();
+        $videos = $this->manager->getRepository(Video::class)->findAll();
 
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+            'videos' => $videos,
         ]);
     }
 }
